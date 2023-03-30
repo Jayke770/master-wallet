1c074c8574ed81e4c00ac0b50ccde0bda7270b71
Jayke770 <jakevelasco40@gmail.com>
2023-03-30 16:02:27 +0800

    🎨 style(create.tsx): increase size of shield icon to 8xl 🚀 chore(create.tsx): add onClick handler to Create Wallet button 🎨 style(create.tsx): add outline to password inputs

--- src/pages/wallet/create.tsx
+++ src/pages/wallet/create.tsx
@@ -2,15 +2,18 @@ import { Button, Card, List, ListInput, Page } from "konsta/react"
 import { Helmet } from "react-helmet"
 import { BsShieldFillCheck } from "react-icons/bs"
 export default function CreateWallet() {
+    const onCreate = () => {
+
+    }
     return (
         <Page className="flex justify-center items-center">
             <Helmet>
-                <title>Master Wallet |Create Wallet</title>
+                <title>Master Wallet | Create Wallet</title>
             </Helmet>
             <Card
                 className="w-full md:w-[400px]">
                 <div className="flex flex-col justify-center items-center gap-3 mt-3">
-                    <BsShieldFillCheck className="text-7xl text-primary" />
+                    <BsShieldFillCheck className="text-8xl text-primary" />
                     <div className="text-2xl px-3.5">Create new wallet</div>
                 </div>
                 <List
@@ -18,14 +21,17 @@ export default function CreateWallet() {
                     margin="m-0">
                     <ListInput
                         floatingLabel
+                        outline
                         label="Password"
                         type="password" />
                     <ListInput
+                        outline
                         floatingLabel
                         label="Confirm Password"
                         type="password" />
                     <div className="px-3.5 mt-2">
-                        <Button>
+                        <Button
+                            onClick={onCreate}>
                             Create Wallet
                         </Button>
                     </div>
